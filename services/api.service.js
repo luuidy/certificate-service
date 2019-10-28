2899d3e857ad473e05f6b037b3b923456c9d2987
Luidy Dias <luidy.informatica@gmail.com>
2019-10-28 04:30:31 -0300

    Init

---
+++ services/api.service.js
@@ -0,0 +1,17 @@
+const ApiGateway = require("moleculer-web");
+
+module.exports = {
+	name: "api",
+	mixins: [ApiGateway],
+
+	settings: {
+		port: process.env.PORT || 3000,
+
+		routes: [{
+			path: "/api",
+			whitelist: [
+				"**"
+			]
+		}],
+	}
+};
