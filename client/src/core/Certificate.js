2317f2dd591dfc449217d11f7276085f10b210b6
luidy <luidy.informaitca@gmail.com>
2019-10-31 17:26:40 -0300

    get certificate

--- client/src/core/Certificate.js
+++ client/src/core/Certificate.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './certificate.css'
 
 const Certificate = () => {
     return (
         <div>
-            <h1>Certificate</h1>
+            <div className="get-certificate">
+            <form>
+                <h1>Get Certificate</h1>
+               <div className="form-group">
+                    <label htmlFor="code-input">Certificate Code</label>
+                    <input type="number" className="form-control" id="code-input"/>
+                </div>
+        
+               
+                <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
