f4fbdc5c5e90751ae7db101bc9b817a05e8a2031
luidyas <luidy.informatica@gmail.com>
2019-11-01 21:36:42 -0300

    onchange and onsubmit

--- client/src/core/Certificate.js
+++ client/src/core/Certificate.js
@@ -4,15 +4,25 @@ import Certification from '../components/Certification'
 
 const Certificate = () => {
     const [certification, setCertification] = useState(null)
+    const [code, setCode] = useState('')
+
+    const change = e => {
+        setCode(e.target.value)
+    }
+
+    const submit = e =>{
+        e.preventDefault()
+        console.log(code)
+    }
     return (
         <div>
             {!certification && (
             <div className="get-certificate">
-            <form>
+            <form onSubmit={submit}>
                 <h1>Get Certificate</h1>
                <div className="form-group">
                     <label htmlFor="code-input">Certificate Code</label>
-                    <input type="number" className="form-control" id="code-input"/>
+                    <input type="number" onChange={change} value={code} className="form-control" id="code-input"/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
